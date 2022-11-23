1b15dc3e27d3fbe2b962c764aa30f8daaf2141da
danipuesdeleo <112347784+danipuesdeleo@users.noreply.github.com>
2022-11-23 14:38:57 -0300

    Create class config.h

---
+++ lib/class config/class config.h
@@ -0,0 +1,30 @@
+#ifndef config_h
+
+class configlib
+{
+    configLib();
+    char *printTest();
+};
+#endif
+
+class config{
+
+    Type type;
+
+    config(){
+
+        type=new Type();
+   
+    
+    }
+    
+    void setType(char* value){
+        type->setType()
+
+    }
+    String getType{
+        type->getType();
+        
+    }
+    
+}
