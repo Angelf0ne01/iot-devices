5ac8ce6fc1be00f04d4c39ceeaab4df68275363e
Lucas <119422978+MartinezLucas65@users.noreply.github.com>
2022-11-29 12:13:39 -0300

    cree la carpeta BME280
    añadi el archivo .h

---
+++ lib/BME280/BME280.h
@@ -0,0 +1,17 @@
+class BME280{
+  private:
+   int pin_21;
+   int pin_22;
+   
+  public:
+   
+   BME280(int nuevo_pin_1, int nuevo_pin_2){
+    pin_21 = nuevo_pin_1;
+    pin_22 = nuevo_pin_2;
+   }
+    
+    String getValue(){
+   
+      return Presion;
+    
+  };
