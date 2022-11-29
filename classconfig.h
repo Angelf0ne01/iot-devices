4d341d7a9db954e534efb90f60ffd6303a93fcf3
MaximodelCastillo <111653854+MaximodelCastillo@users.noreply.github.com>
2022-11-29 15:54:08 -0300

    Update classconfig.h

---
+++ classconfig.h
@@ -0,0 +1,23 @@
+#ifndef config_h
+
+class configlibre
+{
+    configLib();
+    char *printTest();
+};
+#endif
+
+class config{
+    Type type;
+    config(){
+        type=new Type();
+    }
+    
+    void setType(char* value){
+        type->setType()
+
+    }
+    String getType{
+        type->getType(); 
+    }
+};
